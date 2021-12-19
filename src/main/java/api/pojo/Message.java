d44bfa95140a01b2acf62b470d7d602f48c953d4
guneshpatil <guneshpatil@users.noreply.github.com>
2021-12-19 15:59:20 +0530

    Section 9: sample POJO class - Message

---
+++ src/main/java/api/pojo/Message.java
@@ -0,0 +1,22 @@
+package api.pojo;
+
+public class Message {
+    private String message;
+    private String greet;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getGreet() {
+        return greet;
+    }
+
+    public void setGreet(String greet) {
+        this.greet = greet;
+    }
+}
