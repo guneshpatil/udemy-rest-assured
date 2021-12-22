f324a9b21d186f1bcf1636bc1d5cd2fa1e61c6aa
guneshpatil <guneshpatil@users.noreply.github.com>
2021-12-22 15:04:57 +0530

    Section 10: OAuth - commented selenium code as GoogleOAuth has security constraint with selenium login. Code added and tested.

--- src/main/java/api/oauth/GoogleOAuth.java
+++ src/main/java/api/oauth/GoogleOAuth.java
@@ -1,25 +1,50 @@
 package api.oauth;
 
-import com.sun.xml.bind.v2.runtime.reflect.opt.Const;
 import data.Constants;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static api.oauth.OAuthHelper.getCodeOAuthParams;
 import static api.oauth.OAuthHelper.getTokenOAuthParams;
 import static io.restassured.RestAssured.given;
 
 public class GoogleOAuth {
 
-    private String access_token;
+    private String access_token, auth_code;
+
+    @BeforeSuite
+    public void getCode() {
+//        System.setProperty("webdriver.chrome.driver", "C:/Users/IBMADMIN/code/chromedriver.exe");
+//        ChromeOptions chromeOptions = new ChromeOptions();
+////        chromeOptions.addArguments("--incognito");
+//        WebDriver driver = new ChromeDriver(chromeOptions);
+//
+//        driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php");
+//        driver.findElement(By.cssSelector("input[type='email'")).sendKeys(LoginDetails.GOOGLE_USER);
+//        Thread.sleep(1000);
+//        driver.findElement(By.cssSelector("input[type='email'")).sendKeys(Keys.ENTER);
+//        Thread.sleep(5000);
+//        if(driver.findElement(By.cssSelector("[jsname='LgbsSe']")).isDisplayed()){
+//            driver.findElement(By.cssSelector("[jsname='LgbsSe']")).click();
+//            driver.findElement(By.cssSelector("input[type='email'")).sendKeys(LoginDetails.GOOGLE_USER);
+//            Thread.sleep(1000);
+//            driver.findElement(By.cssSelector("input[type='email'")).sendKeys(Keys.ENTER);
+//            Thread.sleep(5000);
+//        }
+//        driver.findElement(By.cssSelector("input[type='password'")).sendKeys(LoginDetails.GOOGLE_PASSWORD);
+//        Thread.sleep(1000);
+//        driver.findElement(By.cssSelector("input[type='password'")).sendKeys(Keys.ENTER);
+//        Thread.sleep(4000);
+//        driver.getCurrentUrl();
+        String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWjjvOlMTITIpINw_Bq7ZSHnC4P7cZWAQqt0NXRDBU47RWyvNqMf7OaiKZuSr6I24g&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=1&prompt=none#";
+//        auth_code = url.split("code=")[1].split("&scope")[0];
+        auth_code = "4%2F0AX4XfWgydfFNmFqriAg5D-hETyWYmEbXkp0w6ptc18bM481KPRTwbmpZWDxh8iVFhbqHJw";
+    }
 
     @Test
-    public void getAccessToken(){
-        access_token = given().queryParams(getTokenOAuthParams())
+    public void getAccessToken() {
+        access_token = given().queryParams(getTokenOAuthParams(auth_code))
+                .log().all()
+                .urlEncodingEnabled(false)
                 .when()
                 .post(Constants.URL_GOOGLE_TOKEN)
                 .then().log().body()
@@ -27,7 +52,7 @@ public class GoogleOAuth {
     }
 
     @Test
-    public void getCourses(){
+    public void getCourses() {
         given().queryParam("access_token", access_token)
                 .when()
                 .get(Constants.URL_RSA_GET_COURSE_PHP)
--- src/main/java/api/oauth/OAuthHelper.java
+++ src/main/java/api/oauth/OAuthHelper.java
@@ -8,21 +8,21 @@ import java.util.Map;
 public class OAuthHelper {
     public static Map<String, String> getCodeOAuthParams() {
         Map<String, String> queryParams = new HashMap<>();
-        queryParams.put("scope","https://www.googleapis.com/auth/userinfo.email");
-        queryParams.put("auth_url","https://accounts.google.com/o/oauth2/v2/auth");
-        queryParams.put("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com");
-        queryParams.put("response_type","code");
-        queryParams.put("redirect_url", Constants.URL_RSA_GET_COURSE_PHP);
+        queryParams.put("scope", "https://www.googleapis.com/auth/userinfo.email");
+        queryParams.put("auth_url", "https://accounts.google.com/o/oauth2/v2/auth");
+        queryParams.put("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com");
+        queryParams.put("response_type", "code");
+        queryParams.put("redirect_uri", Constants.URL_RSA_GET_COURSE_PHP);
         return queryParams;
     }
 
-    public static Map<String, String> getTokenOAuthParams(){
+    public static Map<String, String> getTokenOAuthParams(String auth_code) {
         Map<String, String> queryParams = new HashMap<>();
-        queryParams.put("code","https://www.googleapis.com/auth/userinfo.email");
-        queryParams.put("client_secret","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com");
-        queryParams.put("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com");
-        queryParams.put("grant_type","authorization_code");
-        queryParams.put("redirect_url", Constants.URL_RSA_GET_COURSE_PHP);
+        queryParams.put("code", auth_code);
+        queryParams.put("client_secret", "erZOWM9g3UtwNRj340YYaK_W");
+        queryParams.put("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com");
+        queryParams.put("grant_type", "authorization_code");
+        queryParams.put("redirect_uri", Constants.URL_RSA_GET_COURSE_PHP);
         return queryParams;
     }
 }
